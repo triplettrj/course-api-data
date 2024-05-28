57b0a186ead4c7740b8f0ede71b76e60090bab50
Ryry Triplett <triplettrj@gmail.com>
2024-05-27 23:22:33 -0700

    first commit

---
+++ src/main/java/com/ryrydev/course_api_data/CourseApiDataApplication.java
@@ -0,0 +1,13 @@
+package com.ryrydev.course_api_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ryrydev/course_api_data/TopicRepository.java
@@ -0,0 +1,6 @@
+package com.ryrydev.course_api_data;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+} 
