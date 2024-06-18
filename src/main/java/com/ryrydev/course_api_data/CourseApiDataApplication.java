3e710549ff0e54f61bdc0e330796d059bf72af8e
Ryry Triplett <triplettrj@gmail.com>
2024-06-18 11:47:24 -0700

    Update Derby version and database configuration

--- src/main/java/com/ryrydev/course_api_data/CourseApiDataApplication.java
+++ src/main/java/com/ryrydev/course_api_data/CourseApiDataApplication.java
@@ -3,6 +3,7 @@ package com.ryrydev.course_api_data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CourseApiDataApplication {
 
--- src/main/java/com/ryrydev/course_api_data/entity/Topic.java
+++ src/main/java/com/ryrydev/course_api_data/entity/Topic.java
@@ -2,7 +2,6 @@ package com.ryrydev.course_api_data.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
 public class Topic {
